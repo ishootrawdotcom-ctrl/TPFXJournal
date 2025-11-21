41be9dafbb3301b2321090253b26c8b1910bd114
ishootrawdotcom-ctrl <ishootrawdotcom@gmail.com>
2025-11-21 13:26:29 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, process.cwd(), '');
+  // We cast process to any to avoid TypeScript errors about 'cwd' not existing on Process
+  const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
     define: {
